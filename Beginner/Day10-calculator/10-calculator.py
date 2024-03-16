1f6edd8fcdb7bdbec67fe8c68f37c6868e45e600
Cat Rivas <tykale13@gmail.com>
2024-03-16 17:14:23 -0500

    "Day10 - recursion"

--- Beginner/Day10-calculator/10-calculator.py
+++ Beginner/Day10-calculator/10-calculator.py
@@ -1,12 +1,63 @@
 from art import logo
-import os
-import time
 
-def clear_screen():
-    '''Clears the terminal screen using the 'cls' command on Windows.'''
-    os.system('cls')
+##Calculator operations 
+##Add
+def add(number1, number2):
+    return number1 + number2
+
+##Subtract
+def subtract(number1, number2):
+    return number1 - number2
+
+##Multiply
+def multiply(number1, number2):
+    return number1 * number2
+
+##Divide
+def divide(number1, number2):
+    return number1 / number2
+
+##dictionary in the form of = { '+' : add, ...}
+##save literally the function not the string that contains the name of the function  
+operations = {
+                '+': add, 
+                '-': subtract, 
+                '*': multiply, 
+                '/': divide, 
+}
+
+def calculus():
+    print(logo)
+    first_number = float(input("What's the first number: "))
+    for symbol in operations:
+        print(symbol)
+
+    #flag for the loop
+    keep_calculating = True
+
+    while keep_calculating:
+        operation_symbol = input("Pick an operation: ").strip()
+        second_number = float(input("What's the next number: "))
+
+        operation_choosed = operations[operation_symbol]
+        result = operation_choosed(first_number, second_number)
+
+        print(f'{first_number} {operation_symbol} {second_number} = {result}')
+
+        keep = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start again.: ").lower()
+        
+        #breaking the loop with the flag
+        if keep == 'y':
+            first_number = result
+        else:    
+            keep_calculating = False
+            #recursion
+            calculus()
 
 
-print(logo)
-time.sleep(2)
-clear_screen()
+calculus()
+
+
+
+
+
