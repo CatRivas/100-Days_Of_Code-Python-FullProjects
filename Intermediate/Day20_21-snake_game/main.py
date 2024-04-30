0d9a72d5dccc78af4653599c94eb8f93bc59a5c1
Cat Rivas <tykale13@gmail.com>
2024-04-30 13:03:41 -0500

    snake body

--- Intermediate/Day20_21-snake_game/main.py
+++ Intermediate/Day20_21-snake_game/main.py
@@ -1,4 +1,21 @@
-#i'm back baby
-#stop to learn inheritance
+import turtle
+
+#screen setup
+screen = turtle.Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('black')
+screen.title('Snake Game 30-04-2024')
 
 
+#snake body 
+x_positions = [0, -20, -40]
+
+for position in range(3):
+    sub_snake = turtle.Turtle()
+    sub_snake.shape('square')
+    sub_snake.color('white')
+
+    sub_snake.teleport(x= x_positions[position] , y=0)
+
+
+screen.exitonclick()
