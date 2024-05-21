1d5856763b92dd440930e82a717a7a3b990fc540
Cat Rivas <tykale13@gmail.com>
2024-05-21 12:19:14 -0500

    comments update

--- Intermediate/Day23-turtle_crossing/main.py
+++ Intermediate/Day23-turtle_crossing/main.py
@@ -6,7 +6,7 @@ from scoreboard import Scoreboard
 
 #instance of Screen()
 screen = t.Screen()
-#screen setup
+#screen set up
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('The Turtle Crossing 20-05-24')
@@ -17,8 +17,9 @@ screen.tracer(0)
 #instance of Player()
 player = Player()
 
+#listen for the keyboard
 screen.listen()
-#keyboard
+#up key
 screen.onkey(player.move_up, 'Up')
 
 #instance of CarManager()
@@ -28,46 +29,29 @@ car_manager = CarManager()
 scoreboard = Scoreboard()
 
 
-#flag 
+#game flag 
 is_the_game_on = True
 
 while is_the_game_on:
     time.sleep(0.1)
-    #turns on the animation
+    #turn on the animation
     screen.update()
     
-    #car
+    #create and move the cars
     car_manager.create_car()
     car_manager.car_move()
 
-    #car collision 
+    #car collision?
     for car in car_manager.cars:
         if player.distance(car) < 20:
             is_the_game_on = False
             player.game_over()
 
-
-    #car level up
+    #the player reached the other side? 
     if player.car_colision(): #if this is true
         player.player_go_home()
         car_manager.car_move_incremeant()   
         scoreboard.level_up() 
     
             
-            
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 screen.exitonclick()
