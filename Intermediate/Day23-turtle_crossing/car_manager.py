06e692d862d02ccbcad36b410a008957aca55f3f
Cat Rivas <tykale13@gmail.com>
2024-05-21 12:28:35 -0500

    comments and doctstrings update

--- Intermediate/Day23-turtle_crossing/car_manager.py
+++ Intermediate/Day23-turtle_crossing/car_manager.py
@@ -13,24 +13,26 @@ class CarManager():
 
     def create_car(self):
         random_chance = random.randint(1, 6)
-        #esta condicion ayuda a q no se creen tantos carros a la vez
+        # This condition helps to avoid creating too many cars at once
         if random_chance == 1 :
             new_car = Turtle()
             new_car.shape('square')
             new_car.shapesize(stretch_wid=1, stretch_len=2)
             new_car.color(random.choice(COLORS))
-            #ubicandola
+            #positioning the new car
             new_car.penup()
-            #'y' must be random
+            #the 'y' position must be random
             random_y = random.randint(-250, 250)
             new_car.goto(300, random_y)
-            #metiendo cada carro nuevo en la lista cars
+            #Adding a new car to the cars list
             self.cars.append(new_car)
 
 
     def car_move(self):
+        """Move all cars in the cars list backwards."""
         for car in self.cars:
             car.backward(self.current_speed)
 
     def car_move_incremeant(self):
+        """Increase the speed of the cars."""
         self.current_speed += MOVE_INCREMENT
