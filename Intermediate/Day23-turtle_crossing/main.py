326acaae7a6f12ff1d3b4e6361740381d3723916
Cat Rivas <tykale13@gmail.com>
2024-05-20 19:01:16 -0500

    creating basics

---
+++ Intermediate/Day23-turtle_crossing/main.py
@@ -0,0 +1,22 @@
+import turtle as t
+
+
+
+player = t.Turtle(shape='turtle')
+player.color('white')
+
+screen = t.Screen()
+
+screen.setup(width=600, height=600)
+
+screen.bgcolor('black')
+screen.title('The Turtle Crossing 20-05-24')
+
+
+
+
+
+
+
+
+screen.exitonclick()
