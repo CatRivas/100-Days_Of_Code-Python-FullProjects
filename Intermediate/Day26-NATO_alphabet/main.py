fa51f03d49bd5d4997257cae5c2090a1c9921452
Cat Rivas <tykale13@gmail.com>
2024-09-03 21:33:54 -0500

    NATO with exception handling

--- Intermediate/Day26-NATO_alphabet/main.py
+++ Intermediate/Day26-NATO_alphabet/main.py
@@ -8,28 +8,54 @@ df = pd.read_csv("nato_phonetic_alphabet.csv")
 # ...
 
 
-# row == Series (remember a Series is just an array or list)
-nato_dict = {row.letter: row.code for (index, row) in df.iterrows()}
-# A faster approach is to use .set_index('new index') to set the 'letter' column as an index and remove it as a feature  
-# nato_dict = df.set_index('letter')['code'].to_dict()
+# row == column == Series (remember a Series is just an array or a list)
 
+# Dict comprehension approach with .iterrows() method that iterates over the df in pairs of index, row (Not recommended for larger data sets) 
+nato_dict = {row.letter: row.code for (index, row) in df.iterrows()}
+
+
+# But a faster and more direct approach is to use built in methods of Pandas, as .set_index('new index') to set the 'letter' column as an index and remove it as a feature  (Recommended for larger data sets)
+
+# nato_dict = df.set_index('letter')
+# print(nato_dict)
+#             code
+# letter
+# A           Alfa
+# B          Bravo
+# C        Charlie
+
+
+# nato_dict = df.set_index('letter')['code']
+# print(nato_dict)
+# letter
+# A        Alfa
+# B       Bravo
+# C     Charlie
+
+
+# nato_dict = df.set_index('letter')['code'].to_dict()
+# print(nato_dict)
 # {
 #     "A": "Alfa",
 #     "B": "Bravo",
 #     "C": "Charlie",
-#     "D": "Delta",
-#     "E": "Echo",
-# ...
-#     "U": "Uniform",
-#     "V": "Victor",
-#     "W": "Whiskey",
-#     "X": "X-ray",
-#     "Y": "Yankee",
-#     "Z": "Zulu",
+#      ...
 # }
 
 
-word = input('put a word: ').upper()
 
-list_letters = [nato_dict[letter] for letter in word if letter in nato_dict]
-print(list_letters)
+# list_letters = [nato_dict[letter] for letter in word if letter in nato_dict]
+
+while True:
+    word = input('put a word: ').upper()
+    try:
+        list_letters = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print('Sorry only letter in the alphabet please')
+        continue
+    else:
+        print(list_letters)
+        break
+
+
+
