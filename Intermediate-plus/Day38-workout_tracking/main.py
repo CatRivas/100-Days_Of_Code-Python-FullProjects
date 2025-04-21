80680335372d609b300ce2450f39110e5eedd655
Cat Rivas <tykale13@gmail.com>
2025-04-21 11:45:19 -0500

    sheety connetion done

--- Intermediate-plus/Day38-workout_tracking/main.py
+++ Intermediate-plus/Day38-workout_tracking/main.py
@@ -1,6 +1,8 @@
 import requests
 from dotenv import load_dotenv
 import os
+from datetime import datetime
+
 
 load_dotenv()
 
@@ -27,4 +29,29 @@ parameters = {
 response = requests.post(endpoint, json=parameters, headers=headers)
 response.raise_for_status()
 result = response.json()
-print(result)
+
+exercises_list = result['exercises']
+
+# SHEETY API
+sheety_endpoint = "https://api.sheety.co/4ee484cad71541617520838c5dbd3155/myWorkoutApril2025/workouts"
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+current_time = datetime.now().strftime("%H:%M:%S")
+
+for exercise in exercises_list:    
+    exercise_data_input = {
+        "workout": {
+            "date": today_date,
+            "time":	current_time,
+            "exercise": exercise.get('name').title(),
+            "duration":exercise.get('duration_min'),
+            "calories":exercise.get('nf_calories'),
+        }
+    }
+
+
+    sheety_response = requests.post(sheety_endpoint , json=exercise_data_input)
+    sheety_response.raise_for_status()
+    print(sheety_response.status_code)
+    print(sheety_response.json())
+
